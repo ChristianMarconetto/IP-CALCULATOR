eb731219ef79a92757d3531718f5eacf61679024
Marconetto-Christian <94083692+ChristianMarconetto@users.noreply.github.com>
2023-05-15 21:43:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
+def clearConsole():
+     print("\033c", end='')
 class Address:
-    def __init__(self, input_value):
-        if isinstance(input_value, str):
-            self.__decimal_dotted_quad = input_value
-            self.__binary_dotted = self.__decimal_to_binary(input_value)
-            self.__decimal = self.__binary_to_decimal(self.__binary_dotted)
-        elif isinstance(input_value, int):
-            self.__decimal = input_value
-            self.__binary_dotted = self.__decimal_to_binary(self.__decimal)
-            self.__decimal_dotted_quad = self.__binary_to_decimal(self.__binary_dotted)
+    def __init__(self, inputValue):
+        if isinstance(inputValue, str):
+            self.decimalDotted = inputValue
+            self.binary = self.getBinaryFromDecimalDotted()
+            self.decimal = self.getDecimalFromBinary()
+        if isinstance(inputValue, int):
+            self.decimal = inputValue
+            self.binary = self.getBinaryFromDecimal()
+            self.decimalDotted = self.getDecimalDottedFromBinary()
 
-    def get_decimal_dotted_quad(self):
-        return self.__decimal_dotted_quad
-
-    def get_binary_dotted(self):
-        return self.__binary_dotted
-
-    def get_decimal(self):
-        return self.__decimal
-
-    def __decimal_to_binary(self, decimal_dotted_quad):
-        binary_dotted = ""
-        octets = str(decimal_dotted_quad).split(".")
-        for octet in octets:
-            binary_octet = bin(int(octet))[2:].zfill(8)
-            binary_dotted += binary_octet + "."
-        return binary_dotted[:-1]
-
-    def __binary_to_decimal(self, binary_dotted):
-        decimal_dotted = ""
-        octets = binary_dotted.split(".")
-        for octet in octets:
-            decimal_octet = str(int(octet, 2))
-            decimal_dotted += decimal_octet + "."
-        return decimal_dotted[:-1]
+    def getBinaryFromDecimalDotted(self):
+        binary = self.decimalDotted.split(".")
+        for i in range(4):
+                binary[i] = bin(int(binary[i]))[2:]
+                if len(binary[i])<8:
+                    binary[i] = ((8- len(binary[i] )) * "0" ) + binary[i]
+        return "".join(binary)
 
 
+        
+    def getDecimalFromBinary(self):
+        return int(self.binary,2)
+                   
+    def getBinaryFromDecimal(self):
+        binary = bin(self.decimal)[2:].zfill(32)
+        octets = [binary[i:i+8] for i in range(0, len(binary), 8)]
+        binary_address = ''.join(octets)
+        return binary_address
+    
+    def getDecimalDottedFromBinary(self):
+        binary_address = self.binary  # Ottiene la rappresentazione binaria senza punti
+        octets = [binary_address[i:i+8] for i in range(0, len(binary_address), 8)]  # Divide il numero binario in ottetti
+        decimal_dotted = '.'.join([str(int(octet, 2)) for octet in octets])  # Converte gli ottetti in decimali e li unisce con punti
+        return decimal_dotted
+    
+clearConsole()
+ipDiCasa = Address("192.168.0.1")
+print("IP1 INFO")
+print("Decimal dotted = "+ ipDiCasa.decimalDotted)
+print("Binary = "+ipDiCasa.binary)
+print("Decimal = "+ str(ipDiCasa.decimal))
+print("-------------")
+ipDiScuola = Address(1361052929)
+print("IP2 INFO")
+print("Decimal dotted = "+ ipDiScuola.decimalDotted)#
+print("Binary = "+ipDiScuola.binary)
+print("Decimal = "+ str(ipDiScuola.decimal))
 
-ip = Address("192.168.0.1")
-print("IP1:", ip.get_decimal_dotted_quad())
-print("IP1 binario:", ip.get_binary_dotted())
-print()
-
-ip = Address(-1062731775)
-print("IP2:", ip.get_decimal_dotted_quad())
-print("IP2 binario:", ip.get_binary_dotted())
