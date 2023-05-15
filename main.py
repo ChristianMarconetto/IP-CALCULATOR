77ab42cfaea0692b1d8b8585d314815635f11c43
Marconetto-Christian <94083692+ChristianMarconetto@users.noreply.github.com>
2023-05-15 21:44:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
-def clearConsole():
-     print("\033c", end='')
+import os
+
 class Address:
     def __init__(self, inputValue):
-        if isinstance(inputValue, str):
-            self.decimalDotted = inputValue
-            self.binary = self.getBinaryFromDecimalDotted()
-            self.decimal = self.getDecimalFromBinary()
-        if isinstance(inputValue, int):
-            self.decimal = inputValue
-            self.binary = self.getBinaryFromDecimal()
-            self.decimalDotted = self.getDecimalDottedFromBinary()
+        try:
+            if isinstance(inputValue, str):
+                self.decimalDotted = inputValue
+                self.binary = self.getBinaryFromDecimalDotted()
+                self.decimal = self.getDecimalFromBinary()
+            elif isinstance(inputValue, int):
+                self.decimal = inputValue
+                self.binary = self.getBinaryFromDecimal()
+                self.decimalDotted = self.getDecimalDottedFromBinary()
+            else:
+                raise ValueError("Invalid input type. Must be a string or an integer.")
+        except ValueError as e:
+            print("Error: " + str(e))
+            self.decimalDotted = None
+            self.binary = None
+            self.decimal = None
 
     def getBinaryFromDecimalDotted(self):
-        binary = self.decimalDotted.split(".")
-        for i in range(4):
-                binary[i] = bin(int(binary[i]))[2:]
-                if len(binary[i])<8:
-                    binary[i] = ((8- len(binary[i] )) * "0" ) + binary[i]
-        return "".join(binary)
+        try:
+            binary = [bin(int(octet))[2:].zfill(8) for octet in self.decimalDotted.split(".")]
+            return "".join(binary)
+        except ValueError as e:
+            raise ValueError("Invalid decimal dotted format. " + str(e))
 
-
-        
     def getDecimalFromBinary(self):
-        return int(self.binary,2)
-                   
+        try:
+            return int(self.binary, 2)
+        except ValueError as e:
+            raise ValueError("Invalid binary format. " + str(e))
+
     def getBinaryFromDecimal(self):
-        binary = bin(self.decimal)[2:].zfill(32)
-        octets = [binary[i:i+8] for i in range(0, len(binary), 8)]
-        binary_address = ''.join(octets)
-        return binary_address
-    
+        try:
+            return bin(self.decimal)[2:].zfill(32)
+        except ValueError as e:
+            raise ValueError("Invalid decimal format. " + str(e))
+
     def getDecimalDottedFromBinary(self):
-        binary_address = self.binary  # Ottiene la rappresentazione binaria senza punti
-        octets = [binary_address[i:i+8] for i in range(0, len(binary_address), 8)]  # Divide il numero binario in ottetti
-        decimal_dotted = '.'.join([str(int(octet, 2)) for octet in octets])  # Converte gli ottetti in decimali e li unisce con punti
-        return decimal_dotted
-    
-clearConsole()
+        try:
+            octets = [str(int(self.binary[i:i+8], 2)) for i in range(0, 32, 8)]
+            return ".".join(octets)
+        except ValueError as e:
+            raise ValueError("Invalid binary format. " + str(e))
+
+def clearConsole():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 ipDiCasa = Address("192.168.0.1")
 print("IP1 INFO")
 print("Decimal dotted = "+ ipDiCasa.decimalDotted)
@@ -48,4 +59,3 @@ print("IP2 INFO")
 print("Decimal dotted = "+ ipDiScuola.decimalDotted)#
 print("Binary = "+ipDiScuola.binary)
 print("Decimal = "+ str(ipDiScuola.decimal))
-
